b4abc3765747f992697f2cd52347b6f5f0cf9002
caxky <k_james7@outlook.com>
2021-05-11 17:46:10 -0400

    Gets list of top 10 hot posts from specified subreddits

--- reddit.py
+++ reddit.py
@@ -1,5 +1,6 @@
 import praw
 import configparser
+import pandas as pd
 
 parser = configparser.RawConfigParser()
 parserfilepath = r'config.ini'
@@ -9,8 +10,18 @@ client_id = parser.get('reddit-login', 'client_id')
 client_secret = parser.get('reddit-login', 'client_secret')
 user_agent = parser.get('reddit-login', 'user_agent')
 
+subreddits = [sub.strip() for sub in parser.get('subreddits', 'subreddits').split(',')]
+
 reddit = praw.Reddit(
     client_id=client_id,
     client_secret=client_secret,
     user_agent=user_agent
 )
+
+posts = []
+
+for currentsub in subreddits:
+    for post in reddit.subreddit(currentsub).hot(limit=10):
+        posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+
+posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
