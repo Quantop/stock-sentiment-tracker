8df0e894e38d59c5d55bedea1a10ff395be49ba6
caxky <k_james7@outlook.com>
2021-05-13 17:09:29 -0400

    Extract tickers from comment text

--- analyze.py
+++ analyze.py
@@ -1,12 +1,15 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
+import reticker
 
 reddit_data = pd.read_csv('datasets/comments.csv', usecols=["comment"])
 comments = reddit_data.comment.to_list()
 
 analyzer = SentimentIntensityAnalyzer()
+ticker_extractor = reticker.TickerExtractor()
 
 scores = []
+tickers = []
 for comment in comments:
     sentiment_score = 0
     try:
@@ -14,8 +17,12 @@ for comment in comments:
     except TypeError:
         sentiment_score = 0
 
+    extracted_tickers = ticker_extractor.extract(comment)
+
     scores.append(sentiment_score)
+    tickers.append(extracted_tickers)
 
 reddit_data['sentiment score'] = scores
+reddit_data['tickers'] = tickers
 
 reddit_data.to_csv('datasets/simple_analysis.csv', index=True)
