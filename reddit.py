34989be899f9240e94575ba44b9aa2d1fd8e4c86
caxky <k_james7@outlook.com>
2021-05-13 16:35:27 -0400

    Allow changes of posts per sub and morecomments limit through config

--- reddit.py
+++ reddit.py
@@ -23,8 +23,11 @@ posts = []
 postids = []
 comments = []
 
+posts_per_sub = parser.get('options', 'posts_per_sub')
+morecomments_limit = parser.get('options', 'morecomments_limit')
+
 for currentsub in subreddits:
-    for post in reddit.subreddit(currentsub).hot(limit=2):
+    for post in reddit.subreddit(currentsub).hot(limit=posts_per_sub):
         posts.append([post.id, post.title, post.score, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
         postids.append(post.id)
 
@@ -33,7 +36,7 @@ posts.to_csv('datasets/posts.csv', index=True)
 
 for id in postids:
     submission = reddit.submission(id)
-    submission.comments.replace_more(limit=5)
+    submission.comments.replace_more(limit=morecomments_limit)
 
     for comment in submission.comments.list():
         comments.append([comment.id, comment.parent_id, id, submission.title, comment.body])
