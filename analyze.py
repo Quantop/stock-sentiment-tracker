71f08a6f38047f9f4995bd5536523a5f07849f8b
caxky <k_james7@outlook.com>
2021-05-13 17:47:34 -0400

    Check that ticker mentioned in comments are valid

--- analyze.py
+++ analyze.py
@@ -2,8 +2,25 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 import reticker
 
+def verify_tickers(tickers_list, ticker_data):
+    verified = []
+    ticker_data_set = set(ticker_data)
+
+    for tickers in tickers_list:
+        tickers_set = set(tickers)
+
+        if tickers_set:
+            verified.append(list(tickers_set & ticker_data_set))
+        else:
+            verified.append([])
+
+    return verified
+
+
 reddit_data = pd.read_csv('datasets/comments.csv', usecols=["comment"])
-comments = reddit_data.comment.to_list()
+comments = reddit_data.comment.tolist()
+ticker_data = pd.read_csv('tickers.csv', usecols=["tickers"])
+ticker_data = ticker_data.tickers.tolist()
 
 analyzer = SentimentIntensityAnalyzer()
 ticker_extractor = reticker.TickerExtractor()
@@ -22,7 +39,9 @@ for comment in comments:
     scores.append(sentiment_score)
     tickers.append(extracted_tickers)
 
+verified_tickers = verify_tickers(tickers, ticker_data)
+
 reddit_data['sentiment score'] = scores
-reddit_data['tickers'] = tickers
+reddit_data['tickers'] = verified_tickers
 
 reddit_data.to_csv('datasets/simple_analysis.csv', index=True)
