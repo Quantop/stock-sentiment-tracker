e6527ea9cbf891a872492d49fe8d34c531bf34ce
caxky <k_james7@outlook.com>
2021-05-17 16:22:58 -0400

    Store data only from news portion, ignoring blogs

--- news.py
+++ news.py
@@ -1,10 +1,22 @@
 import scrapy
 import pandas as pd
 
+class NewsItem(scrapy.Item):
+    headline = scrapy.Field()
+    link = scrapy.Field()
+
 class NewsSpider(scrapy.Spider):
     name = 'news_spider'
     allowed_domains = ['finviz.com']
     start_urls = ['https://finviz.com/news.ashx']
 
     def parse(self, response, **kwargs):
-        self.logger.info(response.xpath('//a[@class="nn-tab-link"]'))
+        news_items = []
+        news_rows = response.xpath('//*[@class="news"]/table//descendant::td[1]//tr')
+
+        for row in news_rows:
+            item = NewsItem()
+            item['headline'] = row.xpath('td[3]/a/text()')
+            item['link'] = row.xpath('td[3]/a/@href')
+
+            news_items.append(item)
