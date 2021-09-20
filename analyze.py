6cbfc62816e4b30170878101b80a92dea03c64aa
caxky <k_james7@outlook.com>
2021-09-19 21:05:23 -0400

    Basic sentiment analysis for tweets collected

--- analyze.py
+++ analyze.py
@@ -33,17 +33,16 @@ def process_sentiment(sentiment_datatable):
 
     return general_sentiment/row_count
 
+analyzer = SentimentIntensityAnalyzer()
+ticker_extractor = reticker.TickerExtractor()
 
+# calculates the sentiment on each comment made on reddit
 reddit_data = pd.read_csv('datasets/comments.csv', usecols=["comment"])
 comments = reddit_data.comment.tolist()
 ticker_data = pd.read_csv('tickers.csv', usecols=["tickers"])
 ticker_data = ticker_data.tickers.tolist()
 
-analyzer = SentimentIntensityAnalyzer()
-ticker_extractor = reticker.TickerExtractor()
-
-# calculates the sentiment on each comment made on reddit
-scores = []
+reddit_scores = []
 tickers = []
 for comment in comments:
     sentiment_score = 0
@@ -54,15 +53,34 @@ for comment in comments:
 
     extracted_tickers = ticker_extractor.extract(comment)
 
-    scores.append(sentiment_score)
+    reddit_scores.append(sentiment_score)
     tickers.append(extracted_tickers)
 
 verified_tickers = verify_tickers(tickers, ticker_data)
 
-reddit_data['sentiment score'] = scores
+reddit_data['sentiment score'] = reddit_scores
 reddit_data['tickers'] = verified_tickers
 
+
+# calculates the sentiment on each comment made on reddit
+twitter_data = pd.read_csv('datasets/tweets.csv', usecols=["text"])
+tweets = twitter_data.comment.tolist()
+
+twitter_scores = []
+for tweet in tweets:
+    sentiment_score = 0
+    try:
+        sentiment_score = sentiment_score + analyzer.polarity_scores(tweet)['compound']
+    except TypeError:
+        sentiment_score = 0
+
+    extracted_tickers = ticker_extractor.extract(tweet)
+
+    twitter_scores.append(sentiment_score)
+    tickers.append(extracted_tickers)
+
 # outputs the sentiment results to a datasheet
-reddit_data.to_csv('datasets/simple_analysis.csv', index=True)
+reddit_data.to_csv('datasets/simple_analysis_reddit.csv', index=True)
+twitter_data.to_csv('datasets/simple_analysis_twitter.csv', index=True)
 
 general_sentiment = process_sentiment(reddit_data)
