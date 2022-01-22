f6b07d8f8134392a6fdc13c2a152b54de50a7279
caxky <k_james7@outlook.com>
2022-01-22 15:55:01 -0500

    Changed 'Home' to 'Testing' on navbar

--- frontend-vue/src/components/Navbar.vue
+++ frontend-vue/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div>
    <b-navbar type="dark" variant="dark">
       <b-navbar-nav>
-         <b-nav-item to="/">Home</b-nav-item>
+         <b-nav-item to="/test">Testing</b-nav-item>
          <b-nav-item to="/sentiment">Sentiment</b-nav-item>
       </b-navbar-nav>
    </b-navbar>
