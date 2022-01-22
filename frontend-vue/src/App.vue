3214fd3c42b6279f597594c952dc21a77f932e8d
caxky <k_james7@outlook.com>
2022-01-22 14:53:28 -0500

    Basic navbar

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <navbar></navbar>
+    </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    navbar: Navbar,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ frontend-vue/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+   <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+         <b-nav-item to="/">Home</b-nav-item>
+         <b-nav-item to="/sentiment">Sentiment</b-nav-item>
+      </b-navbar-nav>
+   </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
