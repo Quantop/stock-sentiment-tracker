f3d53a08e8f6e275d07d19fa183dd03911f15312
caxky <k_james7@outlook.com>
2022-01-22 15:53:48 -0500

    Removed unneeded files

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import StockSentiment from '@/views/StockSentiment.vue';
-import Ping from '../components/Ping.vue';
 import Test from '../components/Test.vue';
 
 Vue.use(Router);
@@ -10,16 +9,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Ping,
-    },
-    {
-      path: '/ping',
-      name: 'Ping',
-      component: Ping,
-    },
     {
       path: '/test',
       name: 'Testing',
